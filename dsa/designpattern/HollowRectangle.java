8b5a83ebb09ee06c87a8dddf5981f74c3fd2291b
Hitesh Bhattad <hiteshbhattad2006@gmail.com>
2026-09-25 23:55:09 +0530

    Create HollowRectangle.java

---
+++ dsa/designpattern/HollowRectangle.java
@@ -0,0 +1,23 @@
+public class HollowRectangle {
+    public static void hollowPattern(int lines,int cols) {
+        for(int i=1; i<=lines; i++)
+        {
+            for(int j=1; j<=cols; j++)
+            {
+                if(i == 1 || j == 1 || i == lines || j == cols)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void main(String[] args) {
+        hollowPattern(7,15);
+    }
+}
